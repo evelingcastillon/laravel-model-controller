48bf6d20382eb903d50b8eb8be5050170a1ec18f
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-13 10:11:13 +0200

    aggiunto titoli

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="movies">
+                @foreach ($movies as $movie)
+                {{ $movie->title }}
+                @endforeach
+            </div>
+        </div>
+    </body>
+</html>
